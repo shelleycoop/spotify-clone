662036092c5ea0a53d1472162d28fa2ba7888775
shelleycoop <sanjeevk9335@gmail.com>
2024-01-03 20:16:56 +0530

    day 2 commit 1

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -5,6 +5,8 @@ import { useMemo } from "react";
 import {HiHome} from "react-icons/hi"
 import {BiSearch} from "react-icons/bi" //14:30
 import Box from "./Box"; //17:09
+import SidebarItem from "./SidebarItem";
+import Library from "./Library";
 
 interface SidebarProps {
     children: React.ReactNode;   
@@ -42,15 +44,34 @@ const Sidebar: React.FC<SidebarProps> = ({children}) => {
             "
             >
                 <Box>
-                    Sidebar Navigation
-                </Box>
-                <Box className="overflow-y-auto h-full">
-                    Song Library
+                    <div
+                    className="
+                    flex
+                    flex-col
+                    gap-y-4
+                    px-5
+                    py-4
+                    "
+                    >
+                    {routes.map((item)=> (
+                        <SidebarItem
+                        key={item.label}
+                        {...item}//20:16
+                        />
+                    ))}
+                    </div>
+                </Box> 
+                <Box className="overflow-y-auto h-full"> 
+                    <Library/>
                 </Box>
 
             </div>
+            <main className="h-full flex-1 overflow-y-auto py-2">
+                {children}
+            </main>
         </div>
     )
 }
 
 export default Sidebar;
+//19:28
